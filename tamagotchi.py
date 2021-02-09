c03707b9067e16bbe822005a6e7de9f8fb7bb314
yungdeni <asertyx@gmail.com>
2021-02-09 09:43:00 +0100

    Class structure for a tamagotchi

---
+++ tamagotchi.py
@@ -0,0 +1,21 @@
+class Tamagotchi:
+    def __init__(self,name,birthday):
+        self.name = name
+        self.hunger = 100
+        self.energy = 100
+        self.birthday = birthday
+    def update(self):
+        1 + 1
+        #hunger function()
+        #energy function()
+
+    
+    def _hunger(self):
+        self.hunger -= 1
+        #hunger logic
+
+    def _energy(self):
+        1 + 1
+        #energy logic
+
+
